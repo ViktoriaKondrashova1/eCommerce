a18bf57e3644d87ce1bb0ea1128e5db6b712bcc0
Viktoria Kondrashova <kavidmi@gmail.com>
2025-04-30 22:52:22 +0300

    feat: add app pages

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react'
+
+export const AboutPage: FC = () => {
+  return (
+    <>
+
+    </>
+  )
+}
---
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react'
+
+export const CatalogPage: FC = () => {
+  return (
+    <>
+
+    </>
+  )
+}

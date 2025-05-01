84e18da3b7850498823df584876f2fbfd256e336
Viktoria Kondrashova <kavidmi@gmail.com>
2025-05-01 18:54:50 +0300

    feat: add Backdrop component

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,14 +1,14 @@
 import type { FC } from 'react'
 import { AppText } from '@/components/AppText/AppText'
 import { AppTitle } from '@/components/AppTitle/AppTitle'
+import { Backdrop } from '@/components/Backdrop/Backdrop'
 import { LoginForm } from '@/components/LoginForm/LoginForm'
-import { Card } from 'antd'
 import { Link } from 'react-router-dom'
 
 export const LoginPage: FC = () => {
   return (
     <>
-      <Card style={{ maxWidth: 400, margin: '0 auto' }}>
+      <Backdrop>
         <AppTitle level={2} style={{ textAlign: 'center' }}>Login</AppTitle>
         <LoginForm />
         <div style={{ textAlign: 'center' }}>
@@ -18,7 +18,7 @@ export const LoginPage: FC = () => {
             <Link to="/register">Register</Link>
           </AppText>
         </div>
-      </Card>
+      </Backdrop>
     </>
   )
 }

9b8fdae75a3e5192543081e7a45925c5191686ac
Viktoria Kondrashova <kavidmi@gmail.com>
2025-05-03 17:48:17 +0300

    feat: add promocode section

---
+++ src/components/PromocodeSection/PromocodeSection.tsx
@@ -0,0 +1,28 @@
+import type { FC } from 'react'
+import { promocode, promocodeText } from '@/shared/constants'
+import { Card, Space } from 'antd'
+import { AppButton } from '../AppButton'
+import { AppText } from '../AppText/AppText'
+import { AppTitle } from '../AppTitle/AppTitle'
+
+export const PromocodeSection: FC = () => {
+  return (
+    <Card
+      style={{
+        background: '#cfd1c6',
+        textAlign: 'center',
+        padding: '15px',
+      }}
+    >
+      <Space direction="vertical" size="small">
+        <AppTitle level={4}>{promocodeText}</AppTitle>
+        <Space>
+          <AppText strong style={{ fontSize: 18, backgroundColor: 'white', padding: '4px 10px', borderRadius: 4 }}>
+            {promocode}
+          </AppText>
+          <AppButton type="primary">Copy Code</AppButton>
+        </Space>
+      </Space>
+    </Card>
+  )
+}
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,15 @@
 import type { FC } from 'react'
 import { HeroSection } from '@/components/HeroSection/HeroSection'
 import { NewProducts } from '@/components/NewProducts/NewProducts'
+import { PromocodeSection } from '@/components/PromocodeSection/PromocodeSection'
 import { Flex } from 'antd'
 
 export const MainPage: FC = () => {
   return (
-    <Flex vertical gap="large">
+    <Flex vertical gap={32}>
       <HeroSection />
       <NewProducts />
+      <PromocodeSection />
     </Flex>
   )
 }

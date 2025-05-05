1235fd1be5607f74339529e04093cd0ad082071d
EvgenKh <hamitcevich@gmail.com>
2025-05-05 11:52:13 +0300

    feat: add address field to form

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,8 +1,9 @@
 import type { FC } from 'react'
-import { DatePicker, Form, Input } from 'antd'
+import { DatePicker, Divider, Form, Input } from 'antd'
 import { useState } from 'react'
+import { AppButton } from '../AppButton'
 import { AppInput } from '../AppInput/AppInput'
-import { confirmPasswordValidationRules, dateValidationRules, emailValidationRules, nameValidationRules, passwordValidationRules } from './index'
+import { cityValidationRules, confirmPasswordValidationRules, countryValidationRules, dateValidationRules, emailValidationRules, nameValidationRules, passwordValidationRules, postalCodeValidationRules, streetValidationRules } from './index'
 
 export const RegisterForm: FC = () => {
   const [passwordVisible, setPasswordVisible] = useState<boolean>(false)
@@ -29,14 +30,47 @@ export const RegisterForm: FC = () => {
           label="Date of birth"
           rules={dateValidationRules}
         >
-          <DatePicker style={{ width: '100%' }} />
+          <DatePicker
+            style={{ width: '100%' }}
+            placeholder="1964-04-01"
+          />
         </Form.Item>
+        <Divider>Address</Divider>
+        <Form.Item
+          name="country"
+          label="Your country"
+          rules={countryValidationRules}
+        >
+          <AppInput placeholder="Belarus" />
+        </Form.Item>
+        <Form.Item
+          name="city"
+          label="Your city"
+          rules={cityValidationRules}
+        >
+          <AppInput placeholder="Minsk" />
+        </Form.Item>
+        <Form.Item
+          name="postalCode"
+          label="Postal code"
+          rules={postalCodeValidationRules}
+        >
+          <AppInput placeholder="220004" />
+        </Form.Item>
+        <Form.Item
+          name="street"
+          label="Your street"
+          rules={streetValidationRules}
+        >
+          <AppInput placeholder="Nemiga street" />
+        </Form.Item>
+        <Divider></Divider>
         <Form.Item
           name="email"
           label="Email"
           rules={emailValidationRules}
         >
-          <AppInput placeholder="Email" />
+          <AppInput placeholder="john@gmail.com" />
         </Form.Item>
         <Form.Item
           name="password"
@@ -60,6 +94,11 @@ export const RegisterForm: FC = () => {
             placeholder="Confirm password"
             visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
           />
+        </Form.Item>
+        <Form.Item>
+          <AppButton block type="primary" htmlType="submit">
+            Sign up
+          </AppButton>
         </Form.Item>
       </Form>
     </>

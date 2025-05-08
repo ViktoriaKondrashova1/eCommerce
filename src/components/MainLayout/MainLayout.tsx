0df73ffe35c03705fed02a9194e2df07532932f7
Viktoria Kondrashova <kavidmi@gmail.com>
2025-05-08 21:22:02 +0300

    refactor: refactor MainLayout and App.tsx

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react'
-import { App as AntApp, Layout } from 'antd'
+import { Layout } from 'antd'
 import { Outlet } from 'react-router-dom'
 import { AppFooter } from '../AppFooter/AppFooter'
 import { AppHeader } from '../AppHeader/AppHeader'
@@ -8,19 +8,17 @@ const { Content } = Layout
 
 export const MainLayout: FC = () => {
   return (
-    <AntApp>
-      <Layout style={{ minHeight: '100vh' }}>
-        <AppHeader></AppHeader>
-        <Content style={{
-          margin: '24px 16px',
-          padding: 24,
-          minHeight: 280,
-        }}
-        >
-          <Outlet />
-        </Content>
-        <AppFooter></AppFooter>
-      </Layout>
-    </AntApp>
+    <Layout style={{ minHeight: '100vh' }}>
+      <AppHeader />
+      <Content style={{
+        margin: '24px 16px',
+        padding: 24,
+        minHeight: 280,
+      }}
+      >
+        <Outlet />
+      </Content>
+      <AppFooter />
+    </Layout>
   )
 }

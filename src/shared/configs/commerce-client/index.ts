3c6a2c6267799a9dcb133f7c48ce35158e3c78c7
Alina <lnrzhkv3367337@gmail.com>
2025-05-12 16:51:33 +0300

    feat: add client builder without logger, add corresponding comments to implementation

--- src/shared/configs/commerce-client/index.ts
+++ src/shared/configs/commerce-client/index.ts
@@ -8,22 +8,34 @@ import {
 import { getAuthMiddlewareOptions } from './auth-middleware'
 import { getHttpMiddlewareOptions } from './http-middleware'
 
+// в commerceApiClient храним созданный апи, чтоьбы не создавать его заново при каждом вызове
 let commerceApiClient: ByProjectKeyRequestBuilder | null = null
 
 function getCommerceClient(): ByProjectKeyRequestBuilder {
+  // проверяем существует ли commerceApiClient, если да, то возвращем его
   if (commerceApiClient) {
     return commerceApiClient
   }
-
+  // получаем из commercetools sdk пармаетры для авторизации и http запросов
   const authMiddlewareOptions: AuthMiddlewareOptions = getAuthMiddlewareOptions()
   const httpMiddlewareOptions: HttpMiddlewareOptions = getHttpMiddlewareOptions()
 
-  const ctpClient = new ClientBuilder()
+  // билдер по созданию клиента коммерстулза с нужными мидлвэйрами (автор-ция, http и логирование)
+  const ctpClientWithLogger = new ClientBuilder()
     .withClientCredentialsFlow(authMiddlewareOptions)
     .withHttpMiddleware(httpMiddlewareOptions)
     .withLoggerMiddleware()
     .build()
 
+  // билдер по созданию клиента коммерстулза с нужными мидлвэйрами без логирования
+  const ctpClientWithoutLogger = new ClientBuilder()
+    .withClientCredentialsFlow(authMiddlewareOptions)
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .build()
+
+  const ctpClient = import.meta.env.MODE === 'development' ? ctpClientWithLogger : ctpClientWithoutLogger
+
+  // создаем апи-клиента, привязывапя его к ключу из енва VITE_CTP_KEY
   commerceApiClient = createApiBuilderFromCtpClient(ctpClient)
     .withProjectKey({ projectKey: import.meta.env.VITE_CTP_KEY ?? '' })
 

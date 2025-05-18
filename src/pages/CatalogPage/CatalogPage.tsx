b431aa5686ab38909cc1fabc48b2e5d8e4d4904a
Viktoria Kondrashova <kavidmi@gmail.com>
2025-05-18 18:10:04 +0300

    update: update CatalogPage

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,6 +1,8 @@
 import type { ICleanProduct } from '@/entities/product/model/product.types'
 import type { FC } from 'react'
 import { AppBreadcrumb } from '@/components/AppBreadcrumb/AppBreadcrumb'
+import { AppEmpty } from '@/components/AppEmpty/AppEmpty'
+import { AppSkeleton } from '@/components/AppSkeleton/AppSkeleton'
 import { CatalogPagination } from '@/components/CatalogPagination/CatalogPagination'
 import { CatalogSearch } from '@/components/CatalogSearch/CatalogSearch'
 import { CatalogSidebar } from '@/components/CatalogSidebar/CatalogSidebar'
@@ -8,28 +10,33 @@ import { ProductList } from '@/components/ProductList/ProductList'
 import { fetchProducts } from '@/entities/product/api/fetch-products'
 import { importProductAdapter, useCategories } from '@/shared/adapters/import/product.adapter'
 import { catalogPageLimit } from '@/shared/constants'
+import { useRequest } from '@/shared/hooks/useRequest'
 import { Flex } from 'antd'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export const CatalogPage: FC = () => {
-  const [products, setProducts] = useState<ICleanProduct[]>([])
-  const [total, setTotal] = useState<number | undefined>(0)
   const [currentPage, setCurrentPage] = useState<number>(1)
 
   useCategories()
 
-  useEffect(() => {
-    fetchProducts(currentPage)
-      .then((response) => {
-        const clearProducts = importProductAdapter(response.body.results)
-        setProducts(clearProducts)
-        setTotal(response.body.total)
-      })
-      .catch(() => {
-        throw new Error('clean data not found')
-      },
-      )
+  const fetchProductsForPage = useCallback(async (): Promise<{
+    products: ICleanProduct[]
+    total: number
+  }> => {
+    const response = await fetchProducts(currentPage)
+    const total = response.body.total ?? 0
+
+    return {
+      products: importProductAdapter(response.body.results),
+      total,
+    }
   }, [currentPage])
+
+  const {
+    data: productsData,
+    isLoading,
+    isError,
+  } = useRequest<{ products: ICleanProduct[], total: number }>(fetchProductsForPage)
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
@@ -47,10 +54,25 @@ export const CatalogPage: FC = () => {
       </Flex>
       <Flex gap="large" style={{ marginTop: 40 }}>
         <CatalogSidebar />
-        <Flex vertical gap="large">
-          <ProductList products={products} />
-          <CatalogPagination total={total} pageLimit={catalogPageLimit} current={currentPage} onChange={handlePageChange} />
-        </Flex>
+        {isLoading
+          ? (
+              <AppSkeleton />
+            )
+          : isError
+            ? (
+                <AppEmpty />
+              )
+            : (
+                <Flex vertical gap="large">
+                  <ProductList products={productsData?.products || []} />
+                  <CatalogPagination
+                    total={productsData?.total !== undefined ? productsData.total : 0}
+                    pageLimit={catalogPageLimit}
+                    current={currentPage}
+                    onChange={handlePageChange}
+                  />
+                </Flex>
+              )}
       </Flex>
     </>
   )

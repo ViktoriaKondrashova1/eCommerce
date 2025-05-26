c2de67d17fd200cf4c5c9e99c519f34eec736c0a
Alina <lnrzhkv3367337@gmail.com>
2025-05-27 01:04:01 +0300

    feat: add StatePersonalInfo customer types

--- src/components/Profile/model/types.ts
+++ src/components/Profile/model/types.ts
@@ -1,4 +1,8 @@
-import type { CustomerSignInResult } from '@commercetools/platform-sdk'
+import type { Customer, CustomerSignInResult } from '@commercetools/platform-sdk'
+import type { Dayjs } from 'dayjs'
 
 export type Profile = CustomerSignInResult['customer']
 export type FormMode = 'view' | 'edit'
+export interface StatePersonalInfo extends Pick<Customer, 'firstName' | 'lastName' | 'email'> {
+  dateOfBirth: string | Dayjs
+}

24d261652e25ae2beb14b261a11002aadca600db
Alina <lnrzhkv3367337@gmail.com>
2025-05-26 21:00:11 +0300

    refactor: update eslint formater in Backdrop component

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -1,16 +1,17 @@
-import type { BaseComponent } from '@/shared/types/common.types'
 import type { FC, ReactNode } from 'react'
+import type { BaseComponent } from '@/shared/types/common.types'
 
 import './Backdrop.scss'
 
 interface Props extends BaseComponent {
   children: ReactNode
   className?: string
+
 }
 
-export const Backdrop: FC<Props> = ({ testId = 'backdrop', className = 'backdrop', children }) => {
+export const Backdrop: FC<Props> = ({ testId = 'backdrop', className = 'backdrop', children, ...rest }) => {
   return (
-    <div data-testid={testId} className={className}>
+    <div data-testid={testId} className={className} {...rest}>
       {children}
     </div>
   )

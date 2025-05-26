2cbc5a7706dbaaf2f2fbe04fd2d4de18bdb2f05f
Alina <lnrzhkv3367337@gmail.com>
2025-05-27 01:02:54 +0300

    refactor: disable 'react-hooks-extra/ no-direct-set-state-in-use-effect'

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ export default antfu({
     'ts/no-unsafe-assignment': 'off',
     'ts/no-unsafe-call': 'off',
     'react-hooks-extra/no-unnecessary-use-prefix': 'off',
+    'react-hooks-extra/ no-direct-set-state-in-use-effect': 'off',
   },
 }, {
   files: ['vite.config.ts'],

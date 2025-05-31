8c74af22ed2dacdb47246f41ed8631b995e32760
Alina <lnrzhkv3367337@gmail.com>
2025-05-31 21:44:59 +0300

    fix: fix vite settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,31 @@ export default mergeConfig(viteConfig, defineConfig({
     globals: true,
     watch: false,
     setupFiles: './src/shared/configs/setupTests.ts',
+
+    exclude: [
+      '**/node_modules/**',
+    ],
+
+    onConsoleLog: (log) => {
+      const warnings = [
+          'React Router will begin wrapping state updates in `React.startTransition` in v7.',
+          'React Router Future Flag Warning',
+        'Instead, add a ref directly to the element you want to reference',
+        'Warning: An update to ForwardRef inside a test was not wrapped in act(...)']
+      if (warnings.some(warn => log.includes(warn))) {
+        return false;
+      }
+    },
+
+    fileParallelism: true,
+    pool: 'threads',
+    poolOptions: {
+      threads: {
+        minThreads: 1,
+        maxThreads: 8
+      }
+    }
   },
 
+  cacheDir: './node_modules/.vitest'
 }))

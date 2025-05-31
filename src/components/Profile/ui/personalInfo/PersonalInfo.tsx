1320b5344cf556eb0e8bc4a8f4d494fdc0d42865
Alina <lnrzhkv3367337@gmail.com>
2025-05-31 22:23:28 +0300

    feat: finish personal info user-profile section and corresponding to it make tests

--- src/components/Profile/ui/personalInfo/PersonalInfo.tsx
+++ src/components/Profile/ui/personalInfo/PersonalInfo.tsx
@@ -1,17 +1,24 @@
+import type { ErrorResponse } from '@commercetools/platform-sdk'
 import type { Dayjs } from 'dayjs'
 import type { FC } from 'react'
-import type { StatePersonalInfo } from '@/components/Profile/model/types.ts'
-import { Form } from 'antd'
-import dayjs from 'dayjs'
+import type { FormMode, StatePersonalInfo } from '@/components/Profile/model/types.ts'
+import { EditOutlined } from '@ant-design/icons'
+import { Flex, Form } from 'antd'
 import { observer } from 'mobx-react-lite'
 import { useEffect, useState } from 'react'
-import { ModeAwareContent } from '@/components/Profile/ui/ModeAwareContent.tsx'
+
+import { useParams } from 'react-router-dom'
+import { AppButton } from '@/components/AppButton'
+import { profileAdapter } from '@/components/Profile/model/adapter.ts'
+import { profileService } from '@/components/Profile/model/service.ts'
 import { PersonalInfoEdit } from '@/components/Profile/ui/personalInfo/PersonalInfoEdit.tsx'
 import { PersonalInfoView } from '@/components/Profile/ui/personalInfo/PersonalInfoView.tsx'
-import { updatePersonalInfo } from '@/entities/customer/api/update-personal-info.ts'
 import { customerStore } from '@/entities/customer/model/customer.store.ts'
 import { globalStore } from '@/entities/global/model/global.store.ts'
+import { useNotify } from '@/shared/hooks/use-notify.tsx'
 import { isNonNullable } from '@/shared/types/is-non-nullable.ts'
+import { isNullable } from '@/shared/types/is-nullable.ts'
+import { isType } from '@/shared/types/is-type.ts'
 
 interface Props {}
 
@@ -23,8 +30,11 @@ const initialState = {
 } satisfies StatePersonalInfo
 
 export const PersonalInfo: FC<Props> = observer(() => {
+  const [mode, setMode] = useState<FormMode>('view')
+  const { chapter } = useParams()
   const [formData, setFormData] = useState<StatePersonalInfo>(initialState)
   const [form] = Form.useForm<StatePersonalInfo>()
+  const { showErrorNotify } = useNotify()
 
   const [viewData, setViewData] = useState<StatePersonalInfo>(initialState)
   const [isNeedFormChanged, setIsNeedFormChanged] = useState<boolean>(true)
@@ -47,42 +57,43 @@ export const PersonalInfo: FC<Props> = observer(() => {
     }
   }, [customerStore.customer])
 
+  useEffect(() => {
+    return () => setMode('view')
+  }, [chapter])
+
   const handleClickSave = async () => {
-    const dateOfBirth = dayjs(formData.dateOfBirth).format('YYYY-MM-DD')
-    if (customerStore.customer?.version !== undefined && typeof dateOfBirth === 'string') {
-      globalStore.setLoading(true)
-      updatePersonalInfo({
-        version: customerStore.customer?.version,
-        actions: [
-          {
-            action: 'changeEmail',
-            email: formData.email,
-          },
-          {
-            action: 'setFirstName',
-            firstName: formData.firstName,
-          },
-          {
-            action: 'setLastName',
-            lastName: formData.lastName,
-          },
-          {
-            action: 'setDateOfBirth',
-            dateOfBirth,
-          },
-        ],
-      })
-        .then((res) => {
-          if (res.statusCode === 200) {
-            customerStore.setCustomer(res.body)
-            setIsNeedFormChanged(true)
-          }
-        })
-        .catch(() => {
-          throw new Error('Update date error')
-        })
-        .finally(() => globalStore.setLoading(false))
+    const currentVersion = customerStore.customer?.version
+
+    if (isNullable(currentVersion)) {
+      throw new Error('version is not defined')
     }
+
+    globalStore.setLoading(true)
+
+    void await form.validateFields()
+
+    const userData = profileAdapter.exportUserInfo(formData)
+
+    profileService.updatePersonalInfo({ version: currentVersion, userData })
+      .then((res) => {
+        if (res.statusCode === 200) {
+          customerStore.setCustomer(res.body)
+          setIsNeedFormChanged(true)
+        }
+      })
+      .catch((error) => {
+        if (error !== undefined && isType<ErrorResponse>(error)) {
+          showErrorNotify(error.message)
+        }
+      })
+      .finally(() => globalStore.setLoading(false))
+  }
+
+  const handleClickCancel = () => {
+    setMode('view')
+  }
+  const handleClickEdit = () => {
+    setMode('edit')
   }
 
   const handleChangeForm = (key: keyof StatePersonalInfo, value: string | Dayjs) => {
@@ -92,14 +103,31 @@ export const PersonalInfo: FC<Props> = observer(() => {
     setFormData(() => updatedForm)
   }
 
+  const Component = mode === 'view'
+    ? <PersonalInfoView data={viewData} />
+    : <PersonalInfoEdit data={formData} onChange={handleChangeForm} controller={form} />
+
+  const ButtonBlock = mode === 'view'
+    ? (
+        <Flex justify="end">
+          <AppButton type="primary" onClick={handleClickEdit} icon={<EditOutlined />}>
+            Edit Profile
+          </AppButton>
+        </Flex>
+      )
+    : (
+        <Flex gap={16} justify="end">
+          <AppButton onClick={handleClickCancel}>Cancel</AppButton>
+          <AppButton disabled={isNeedFormChanged} type="primary" onClick={() => void handleClickSave()}>
+            Save
+          </AppButton>
+        </Flex>
+      )
+
   return (
-    <>
-      <ModeAwareContent
-        onClickSave={handleClickSave}
-        isDisabledSaveButton={isNeedFormChanged}
-        viewSlot={<PersonalInfoView data={viewData} />}
-        editSlot={<PersonalInfoEdit data={formData} onChange={handleChangeForm} controller={form} />}
-      />
-    </>
+    <Flex vertical>
+      <div className="profile__content">{Component}</div>
+      <div>{ButtonBlock}</div>
+    </Flex>
   )
 })

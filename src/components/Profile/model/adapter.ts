36dca55ad2c2b3bbd76e74e64146cfeeaa7ba775
Alina <lnrzhkv3367337@gmail.com>
2025-05-31 22:05:27 +0300

    feat: create profileAdapter for adding and updating personal user info and addresses

---
+++ src/components/Profile/model/adapter.ts
@@ -0,0 +1,54 @@
+import type { Address } from '@commercetools/platform-sdk'
+import type { FormDataAddress, StatePersonalInfo } from '@/components/Profile/model/types.ts'
+import dayjs from 'dayjs'
+import { nanoid } from 'nanoid'
+import { getCountryAbbr } from '@/components/Profile/model/lib.ts'
+
+export const profileAdapter = {
+  exportUpdateAddress: (formState: FormDataAddress, controllerValues: FormDataAddress): FormDataAddress => {
+    return {
+      ...formState,
+      country: getCountryAbbr(controllerValues.country),
+    } satisfies FormDataAddress
+  },
+
+  exportAddAddress: (controllerValues: FormDataAddress): FormDataAddress => {
+    return {
+      ...controllerValues,
+      country: getCountryAbbr(controllerValues.country),
+      id: nanoid(),
+    } satisfies FormDataAddress
+  },
+
+  exportUserInfo: (formData: StatePersonalInfo): { email: string, firstName: string, lastName: string, dateOfBirth: string } => {
+    return {
+      email: formData.email,
+      firstName: formData.firstName ?? '',
+      lastName: formData.lastName ?? '',
+      dateOfBirth: dayjs(formData.dateOfBirth).format('YYYY-MM-DD'),
+    }
+  },
+
+  importAddresses: ({ addresses, billingIds }: { addresses: Address[], billingIds: string[] }): FormDataAddress[] => {
+    return addresses?.map((addr) => {
+      const isBillingAddress = billingIds?.some(id => id === addr?.id)
+      const isPrimary
+          = addr.custom?.fields.isPrimary !== undefined
+            ? Boolean(addr.custom?.fields.isPrimary)
+            : false
+
+      return {
+        ...addr,
+        type: isBillingAddress ? 'billing' : 'shipping',
+        isPrimary,
+        custom: {
+          ...addr.custom,
+          type: { key: 'address-custom-field' },
+          fields: {
+            isPrimary: Boolean(addr?.custom?.fields.isPrimary),
+          },
+        },
+      } satisfies FormDataAddress
+    })
+  },
+}

eaa9dec5a583bc249344a247c8c40ed7e5532b3b
Alina <lnrzhkv3367337@gmail.com>
2025-06-08 20:22:23 +0300

    fix: fix cart redirect at header mobile and desktop menu, add cart counter badge

--- src/components/AppHeader/app-header-unauth/HeaderDesktopNav.tsx
+++ src/components/AppHeader/app-header-unauth/HeaderDesktopNav.tsx
@@ -1,17 +1,30 @@
 import type { MenuItem } from '@/components/AppHeader/types'
 import { ShoppingCartOutlined } from '@ant-design/icons'
+import { Badge, Flex } from 'antd'
+import { observer } from 'mobx-react-lite'
+import { useNavigate } from 'react-router-dom'
+import { AppButton } from '@/components/AppButton'
 import { HeaderMenu } from '@/components/AppHeader/HeaderMenu/HeaderMenu'
+import { cartStore } from '@/entities/cart/model/cart.store.ts'
 import { ROUTES } from '@/shared/constants.ts'
 
-export function HeaderDesktopNav() {
+export const HeaderDesktopNav = observer(() => {
+  const navigate = useNavigate()
+
   const menuItems: MenuItem[] = [
     { key: ROUTES.main, label: 'MAIN' },
     { key: '/about', label: 'ABOUT' },
     { key: ROUTES.catalog.default, label: 'CATALOG' },
     { key: '/login', label: 'LOG IN', color: 'primary' },
     { key: '/register', label: 'SIGN UP', color: 'primary' },
-    { key: '/cart', icon: <ShoppingCartOutlined style={{ fontSize: '18px' }} /> },
   ]
 
-  return <HeaderMenu items={menuItems} />
-}
+  return (
+    <Flex>
+      <HeaderMenu items={menuItems} />
+      <Badge showZero size="small" count={cartStore.cart?.totalLineItemQuantity ?? 0}>
+        <AppButton color="default" variant="filled" icon={<ShoppingCartOutlined style={{ fontSize: '18px' }} />} onClick={() => navigate(ROUTES.cart)} />
+      </Badge>
+    </Flex>
+  )
+})
--- src/components/AppHeader/app-header-unauth/HeaderMobileNav.tsx
+++ src/components/AppHeader/app-header-unauth/HeaderMobileNav.tsx
@@ -1,10 +1,12 @@
 import type { MenuItem } from '@/components/AppHeader/types'
-import { MenuOutlined } from '@ant-design/icons'
-import { Divider, Drawer, Flex } from 'antd'
+import { MenuOutlined, ShoppingCartOutlined } from '@ant-design/icons'
+import { Badge, Divider, Drawer, Flex } from 'antd'
 import { observer } from 'mobx-react-lite'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { AppButton } from '@/components/AppButton'
 import { HeaderMenu } from '@/components/AppHeader/HeaderMenu/HeaderMenu'
+import { cartStore } from '@/entities/cart/model/cart.store.ts'
 import { ROUTES } from '@/shared/constants'
 
 export const HeaderMobileNav = observer(() => {
@@ -12,9 +14,9 @@ export const HeaderMobileNav = observer(() => {
     { key: ROUTES.main, label: 'MAIN' },
     { key: '/about', label: 'ABOUT' },
     { key: ROUTES.catalog.default, label: 'CATALOG' },
-    { key: '/cart', label: 'CART' },
   ]
 
+  const navigate = useNavigate()
   const authItems: MenuItem[] = [
     { key: '/login', label: 'LOG IN' },
     { key: '/register', label: 'SIGN UP' },
@@ -28,6 +30,11 @@ export const HeaderMobileNav = observer(() => {
 
   const onClose = () => {
     setIsDrawerOpen(false)
+  }
+
+  const handleClickCart = () => {
+    navigate(ROUTES.cart)
+    onClose()
   }
 
   return (
@@ -45,6 +52,10 @@ export const HeaderMobileNav = observer(() => {
       >
         <Flex vertical>
           <HeaderMenu items={menuItems} align="column" itemStyles={{ minWidth: '100%' }} onItemClick={onClose} />
+          <AppButton color="default" variant="text" icon={<ShoppingCartOutlined style={{ fontSize: '18px' }} />} onClick={handleClickCart}>
+            <Badge showZero size="small" count={cartStore.cart?.totalLineItemQuantity ?? 0} />
+          </AppButton>
+
           <Divider />
           <HeaderMenu items={authItems} align="column" itemStyles={{ minWidth: '100%' }} onItemClick={onClose} />
         </Flex>

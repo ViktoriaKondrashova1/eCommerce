201d578c8d0747ceca95c326cd1b640274309a10
Viktoria Kondrashova <kavidmi@gmail.com>
2025-06-10 23:34:54 +0300

    feat: update loginCustomer

--- src/entities/customer/api/sign-in.ts
+++ src/entities/customer/api/sign-in.ts
@@ -1,14 +1,22 @@
 import type { ClientResponse, CustomerSignInResult, MyCustomerSignin } from '@commercetools/platform-sdk'
 import { cartStore } from '@/entities/cart/model/cart.store'
 import { commerceApi } from '@/shared/configs/commerce-client'
+import { isNonNullable } from '@/shared/types/is-non-nullable'
 
 export async function loginCustomer(loginData: MyCustomerSignin): Promise<ClientResponse<CustomerSignInResult>> {
+  const anonymousCartId = cartStore.getCartId()
+
   try {
     const response = await commerceApi.client
       .me()
       .login()
       .post({
-        body: loginData,
+        body: {
+          ...loginData,
+          activeCartSignInMode: 'MergeWithExistingCustomerCart',
+          ...(isNonNullable(anonymousCartId) && { anonymousCartId }),
+          updateProductData: true,
+        },
       })
       .execute()
 

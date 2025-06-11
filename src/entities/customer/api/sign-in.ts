4fb76c12aee4ad3d02840a6cc92d2920895715fb
Viktoria Kondrashova <kavidmi@gmail.com>
2025-06-11 17:47:17 +0300

    feat: add getCustomerCart

--- src/entities/customer/api/sign-in.ts
+++ src/entities/customer/api/sign-in.ts
@@ -1,20 +1,21 @@
 import type { ClientResponse, CustomerSignInResult, MyCustomerSignin } from '@commercetools/platform-sdk'
 import { cartStore } from '@/entities/cart/model/cart.store'
 import { commerceApi } from '@/shared/configs/commerce-client'
-import { isNonNullable } from '@/shared/types/is-non-nullable'
+import { getCustomerCart } from './get-customer-cart'
 
 export async function loginCustomer(loginData: MyCustomerSignin): Promise<ClientResponse<CustomerSignInResult>> {
-  const anonymousCartId = cartStore.getCartId()
-
   try {
+    const customerCart = await getCustomerCart()
+
     const response = await commerceApi.client
       .me()
       .login()
       .post({
         body: {
           ...loginData,
-          activeCartSignInMode: 'MergeWithExistingCustomerCart',
-          ...(isNonNullable(anonymousCartId) && { anonymousCartId }),
+          activeCartSignInMode: customerCart
+            ? 'MergeWithExistingCustomerCart'
+            : 'UseAsNewActiveCustomerCart',
           updateProductData: true,
         },
       })
